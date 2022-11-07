b9480b7f6ac44040e74309f0e51ae95a7322e57b
Ashishkuldeep23 <ashishkuldeep6@gmail.com>
2022-11-07 20:19:05 +0530

    All Done this branch now

--- src/controllers/problemController.js
+++ src/controllers/problemController.js
@@ -8,25 +8,44 @@ const jwt = require('jsonwebtoken')
 
 // // Regiser new user
 
-const createUserReg = async function(req , res){
-
-    const {firstName , mobile , emailId , password}  = req.body
+const createUserReg = async function (req, res) {
 
 
-    let genderUpdate  = req.body.gender.toLowerCase()
+    try {
+
+        // Required fields extracted from obj -->
+        const { firstName, mobile, emailId, password, gender } = req.body
 
 
-    if(!firstName || !mobile  || !emailId || !password){
-        return res.send("Required feild is not given.")
+        let genderUpdate = req.body.gender.toLowerCase()       // Given data in lower case
+        req.body.gender = genderUpdate      // // Updating  the lower case data into 
+
+
+
+        if (!firstName || !mobile || !emailId || !password || !gender) {
+            return res.status(400).send("Required feild is not given (Mandatory things is not given by user).")
+            // // Mandatory things is not given by user thats why i'm using 400 status code
+        }
+
+
+
+
+        let data = req.body
+
+        let newData = await userModel.create(data)
+        res.status(201).send({ UserAdded: newData })
+
+        // New entry is created thats why i'm using 201 status code
+
+
+
+    } catch (e) {
+        console.log(e.message)
+
+        res.status(500).send({ staus: false, msg: error.message })
     }
-    
 
-    req.body.gender = genderUpdate
 
-    let data = req.body
-
-    let newData = await userModel.create(data)
-    res.send({UserAdded : newData})
 }
 
 
@@ -38,30 +57,44 @@ const createUserReg = async function(req , res){
 
 // User LogIn with email and pass And JWT token creation during logIn   
 
-const userLogIn = async function(req , res){
+const userLogIn = async function (req, res) {
 
-    let email = req.body.emailId 
-    let pass = req.body.password
+    try {
 
-    let getUser = await userModel.findOne({emailId : email , password : pass})
 
-    if(!getUser){
-        return res.send("Give valid Email or Password")
+
+        let email = req.body.emailId
+        let pass = req.body.password
+
+        if (!email || !pass) return res.status(400).send("Required feild is not given (Mandatory things is not given by user).")
+
+        let getUser = await userModel.findOne({ emailId: email, password: pass })
+
+        if (!getUser) {
+            return res.status(400).send("Give valid Email or Password (Given inut is incorrect)")
+        }
+
+
+        let name = getUser.name
+        let id = getUser._id
+
+
+        let token = await jwt.sign({ userName: name, userId: id }, "functionUp-Lithium")
+
+        console.log(name, ":", token)
+
+
+
+
+        res.status(200).send({ staus: true, data: { yourToken: token }, outPut: getUser })
+        // LogIn happend thats why i'm using 200 status code
+
+    } catch (err) {
+        console.log(err.message)
+
+        res.status(500).send({ staus: false, msg: error.message })
     }
 
-
-    let name = getUser.name
-    let id = getUser._id
-
-
-    let token = await jwt.sign({userName : name  , userId : id} , "functionUp-Lithium" )
-
-    console.log(name ,":",token )
-
-
-
-
-    res.send({staus : true , data : { yourToken : token } , outPut : getUser })
 
 }
 
@@ -74,19 +107,31 @@ const userLogIn = async function(req , res){
 
 // // // Fetching only one data with userID
 
-const userFetch = async function(req , res){
+const userFetch = async function (req, res) {
 
-    let id = req.params.userId
-
-    let userData = await userModel.findById(id)
-
-    console.log("Data given to client side")
+    try {
 
 
 
+        let id = req.params.userId
+
+        let userData = await userModel.findById(id)
+
+        console.log("Data given to client side")
 
 
-    res.send({Status : true , OutPut : userData})
+
+
+
+        res.status(200).send({ Status: true, OutPut: userData })
+        // User Fetching data by userId in params and it's own token thats why i'm using 200 status code
+
+    } catch (err) {
+        console.log(err.message)
+
+        res.status(500).send({ staus: false, msg: error.message })
+    }
+
 }
 
 
@@ -99,22 +144,33 @@ const userFetch = async function(req , res){
 
 // // // Updating data with userID for one user
 
-const userUpdate = async function(req , res){
+const userUpdate = async function (req, res) {
 
-    let id = req.params.userId
+    try {
 
-    // console.log(id)
+        let id = req.params.userId
+
+        // console.log(id)
 
 
-    let newUserData = await userModel.findOneAndUpdate(
-        {_id : id} ,
-        {$set : { age : 50 , gender : "other" }} ,
-        {new : true}
-    )
+        let newUserData = await userModel.findOneAndUpdate(
+            { _id: id },
+            { $set: { age: 50, gender: "other" } },
+            { new: true }
+        )
 
-    console.log("Updated data given to client side")
+        console.log("Updated data given to client side")
 
-    res.send({OutPut : newUserData})
+        res.status(200).send({ OutPut: newUserData })
+        // Update happend and every thing is ok thats why i'm using 200 status code
+
+
+    } catch (err) {
+        console.log(err.message)
+
+        res.status(500).send({ staus: false, msg: error.message })
+    }
+
 
 }
 
@@ -128,21 +184,36 @@ const userUpdate = async function(req , res){
 
 // // // Deleting data with userID for one user
 
-const userDelete = async function(req ,res){
-    let id = req.params.userId
+const userDelete = async function (req, res) {
 
-    // console.log(id)
-
-    let newUserData = await userModel.findByIdAndUpdate(
-        {_id : id} ,
-        {$set : {isDeleted : true}} ,
-        {new : true}
-    )
+    try {
 
 
-    console.log("Deleted data given to client side")
 
-    res.send({OutPut : newUserData})
+
+        let id = req.params.userId
+
+        // console.log(id)
+
+        let newUserData = await userModel.findByIdAndUpdate(
+            { _id: id },
+            { $set: { isDeleted: true } },
+            { new: true }
+        )
+
+
+        console.log("Deleted data given to client side")
+
+        res.status(200).send({ OutPut: newUserData })
+        // Delet happend and every thing is ok thats why i'm using 200 status code
+
+    } catch (err) {
+
+        console.log(err.message)
+
+        res.status(500).send({ staus: false, msg: error.message })
+    }
+
 
 }
 
@@ -153,38 +224,58 @@ const userDelete = async function(req ,res){
 
 
 // Making new post -->
-const makePost = async function(req , res){
+const makePost = async function (req, res) {
 
 
-    let id = req.params.userId
-
-    let newPostIs = req.body.posts
+    try {
 
 
-    // // If posts key not present in req.body
-    if(!newPostIs) return res.send("Please give some data in post key.....")
-
-    let uploadPost = await userModel.findById(id)
-
-    // Pushing Data into Array of posts
-    let pushedInPosts = uploadPost.posts.push(newPostIs)
-
-  
-    console.log(uploadPost.posts)
-   
-    // // After pushing data need to updated into DB so doing this -->
-    let updetedData = await userModel.findOneAndUpdate(
-        {_id : id},
-        {$push : {posts : newPostIs}},      // $push is mongoose opr. used as $set op. $push takes arr and newItem in object form.
-        {new:true}
-    )
 
 
-    res.send({
-        status : "Uploaded" ,
-        "Uploaded Post Is" : updetedData.posts[updetedData.posts.length - 1] ,
-        allData : updetedData
-    })
+        let id = req.params.userId
+
+        let newPostIs = req.body.posts
+
+        // // If posts key not present in req.body
+        if (!newPostIs) return res.status(400).send("Please give some data in post key..... (Mandatory field is not given)")
+
+
+
+        let uploadPost = await userModel.findById(id)
+
+        // Pushing Data into Array of posts
+        let pushedInPosts = uploadPost.posts.push(newPostIs)
+
+
+        console.log(uploadPost.posts)
+
+        // // After pushing data need to updated into DB so doing this -->
+        let updetedData = await userModel.findOneAndUpdate(
+            { _id: id },
+            { $push: { posts: newPostIs } },      // $push is mongoose opr. used as $set op. $push takes arr and newItem in object form.
+            { new: true }
+        )
+
+
+        res.status(200).send({
+            status: "Uploaded",
+            "Uploaded Post Is": updetedData.posts[updetedData.posts.length - 1],
+            allData: updetedData
+        })
+
+        // Update happend and every thing is ok thats why i'm using 200 status code
+
+
+    } catch (error) {
+
+        console.log(error.message)
+
+        res.status(500).send({ staus: false, msg: error.message })
+
+    }
+
+
+
 
 }
 
@@ -196,9 +287,25 @@ const makePost = async function(req , res){
 
 // Fetching All user data -->
 
-const allUserData = async function(req ,res){
-    let allData = await userModel.find()
-    res.send({OutPut : allData})
+const allUserData = async function (req, res) {
+
+    try {
+
+
+        let allData = await userModel.find()
+        res.status(200).send({ OutPut: allData })
+        // Fetching data and every thing is ok thats why i'm using 200 status code
+
+    }
+
+    catch (error) {
+        console.log(error.message)
+
+        res.status(500).send({ staus: false, msg: error.message })
+
+    }
+
+
 }
 
 
@@ -208,4 +315,4 @@ const allUserData = async function(req ,res){
 
 
 
-module.exports = {createUserReg , userLogIn ,userFetch ,userUpdate , userDelete , makePost , allUserData}
+module.exports = { createUserReg, userLogIn, userFetch, userUpdate, userDelete, makePost, allUserData }
--- src/middleware/problemMW.js
+++ src/middleware/problemMW.js
@@ -5,22 +5,47 @@ const jwt = require('jsonwebtoken')
 
 // // // Token given or not --------->
 
-const tokenCheck = function(req, res , next){
+const tokenCheck = function (req, res, next) {
 
-    let token = req.headers["x-auth-token"]
+    try {
 
-    console.log(typeof token)
 
-    // console.log(token)
 
-    if(! token){
+        let token = req.headers["x-auth-token"]
 
-        console.log("x-auth-token key is not given")
+        console.log(typeof token)
 
-        return res.send({status : false , msg : "Please give token in Header."})
+        // console.log(token)
+
+        if (!token) {
+
+            console.log("x-auth-token key is not given")
+
+            return res.status(400).send({ status: false, msg: "Please give token in Header. Mandatory thing is not given" })
+            // // Mandatory things is not given by user thats why i'm using 400 status code
+
+        }
+
+
+
+        next()
+
+
+
+
+    } catch (err) {
+        console.log(err.message)
+
+        res.status(500).send({ staus: false, msg: error.message })
     }
 
-    next()
+
+
+
+
+    // next()
+
+
 
 }
 
@@ -29,46 +54,61 @@ const tokenCheck = function(req, res , next){
 
 // // // Token given and now verifing toke with .verify() function --------->
 
-const tokenVerify_Auth = async function(req , res , next){
-    
-    let token = req.headers["x-auth-token"]
+const tokenVerify_Auth = async function (req, res, next) {
 
-    // console.log(token)
 
-    try{
+    try {
 
-        let isTokenValid = await jwt.verify(token , "functionUp-Lithium")
+        let token = req.headers["x-auth-token"]
+
+        // console.log(token)
+
+
+
+        // Authentication part -->
+
+        let isTokenValid = await jwt.verify(token, "functionUp-Lithium")
+        // If this fails then code go into catch block.
 
         // console.log(isTokenValid)   // Printing All Token data 
+
+
+
+
+
+        // Authorization part -->
 
         // UserId from token data -->
         let userIdInTokenEx = isTokenValid.userId
 
         // console.log(userIdInTokenEx)    // Printing userId present in Token Data
 
-        
+
         // // // Checking Authorisation (userId given from params is equal to with token's userId or not ??)
 
         let id = req.params.userId
 
-        if(userIdInTokenEx !== id) return res.send({Status : false , msg : "Not Authorized , Given token is not matched with given userId in pah params" })
+        if (userIdInTokenEx !== id) {
+            return res.status(403).send({ Status: false, msg: "Not Authorized , Given token is not matched with given userId in pah params" })
+        }
 
-
-        // // next()
+        next()
 
         
-    }catch(err){
 
-        if(err){
+
+    } catch (err) {
+
+        if (err) {
             console.log("Error Occurs")
-            return res.send("Given JWT Token is NOT Valid, please give correct jwt token.")
+            return res.status(500).send("Given JWT Token is NOT Valid, please give correct jwt token.")
         }
 
     }
 
 
-    
-    next()
+
+    // next()
 
 
 }
@@ -81,5 +121,5 @@ const tokenVerify_Auth = async function(req , res , next){
 
 
 
-module.exports = {tokenCheck , tokenVerify_Auth}
+module.exports = { tokenCheck, tokenVerify_Auth }
 
